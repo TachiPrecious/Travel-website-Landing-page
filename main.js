72d06dea0e83d9acb1acf5f938b5c574f60fbc54
TachiPrecious <fonguongprecious12@gmail.com>
2026-03-15 14:40:32 +0100

    modified main.js

--- main.js
+++ main.js
@@ -8,11 +8,3 @@ bar.onclick = () => {
     console.log('working')
 }
 
-bar.addEventListener('click', () =>{
-    let mobileNav = document.querySelector('.mobile-nav')
-    console.log(mobileNav);
-})
-
-function toggleNavBar(){ 
-    console.log('working');
-}
